69ec45a950ab468d3b1a15942dd3df1e4befe19e
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-07-06 14:39:18 -0700

    added orm

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  selectAll: function(table, cb) {
+    var queryString = "SELECT * FROM " + table + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table + " (" + cols + ") VALUES (?)";
+    console.log(queryString);
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString =
+      "UPDATE " + table + " SET " + objColVals + " WHERE " + condition;
+    console.log(queryString);
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
