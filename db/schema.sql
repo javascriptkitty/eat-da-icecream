0f2a4a305bf6872e0d4d994c58304baadb606133
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-07-05 21:18:57 -0700

    created db

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ice_cream_DB;
+USE ice_cream_DB;
+
+CREATE TABLE ice_creams
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+name VARCHAR
+    (30) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO ice_creams
+    (name)
+VALUES
+    ('vanilla'),
+    ('green tea'),
+    ('banana'),
+    ('chocolate'),
+    ('strawberry cheesecake'),
+    ('tiramisu'),
+    ('rum rasin'),
+    ('mango'),
+    ('cookie dough'),
+    ('caramel');
