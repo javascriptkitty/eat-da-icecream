72ad50b765e9cd06bcebaf6073566c7dcb7536e6
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-07-09 10:49:20 -0700

    changed the connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "ice_cream_DB"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "ice_cream_DB"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
